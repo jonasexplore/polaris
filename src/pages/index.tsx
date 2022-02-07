57fb2724ce28051682a162a8501e9b20de347c60
Jonas Brito <jonasexplore@gmail.com>
2022-02-07 08:26:52 -0300

    feat(*): adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+import { Button } from 'antd'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push('/auth/signin')
+  }
+
   return (
     <div className={styles.container}>
-      Hello world
+      <Button onClick={onClick} type='primary'>Hello</Button>
     </div>
   )
 }
