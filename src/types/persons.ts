8ec7b133155177680ebed11ee2cdd5d5758c9110
Jonas Brito <jonasexplore@gmail.com>
2022-02-07 08:26:28 -0300

    feat(types): add persons type

---
+++ src/types/persons.ts
@@ -0,0 +1,13 @@
+export type Person = {
+  id?: string;
+  document: string;
+  name: string;
+  email: string;
+  birthDate: Date;
+  phone: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  zip: string;
+};
