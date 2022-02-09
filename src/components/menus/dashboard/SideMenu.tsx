75253dc1e1753a80cb4fd9d425c56864676a7450
Jonas Brito <jonasexplore@gmail.com>
2022-02-09 12:17:42 -0300

    feat(components): adjust side menu

--- src/components/menus/dashboard/SideMenu.tsx
+++ src/components/menus/dashboard/SideMenu.tsx
@@ -1,15 +1,20 @@
-import { UserAddOutlined, BookOutlined } from "@ant-design/icons"
+import { UserAddOutlined, BookOutlined, TableOutlined } from "@ant-design/icons"
 import { Menu } from "antd"
 
 const { SubMenu } = Menu
 
-const SideMenuDashboard = () => {
+type SideMenuProps = {
+  setCurrentSideMenu: any;
+}
+
+const SideMenuDashboard = ({ setCurrentSideMenu }: SideMenuProps) => {
   return (
     <Menu
       mode="inline"
       defaultSelectedKeys={['1']}
       defaultOpenKeys={['Pessoas']}
       style={{ height: '100%', borderRight: 0 }}
+      onClick={(e) => setCurrentSideMenu(e.key)}
     >
       <SubMenu
         key="Pessoas"
@@ -36,6 +41,12 @@ const SideMenuDashboard = () => {
         <Menu.Item key="4">Casamento</Menu.Item>
         <Menu.Item key="5">Óbito</Menu.Item>
       </SubMenu>
+      <Menu.Item key="6">
+        <span>
+          <TableOutlined style={{ marginRight: '0.5rem' }} />
+          Emolumentos
+        </span>
+      </Menu.Item>
     </Menu>
   )
 }
