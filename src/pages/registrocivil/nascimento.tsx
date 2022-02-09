7956937e90d41fea0489bbbba57597c69d69fa98
Jonas Brito <jonasexplore@gmail.com>
2022-02-09 12:20:22 -0300

    feat(pages): add birth pages

---
+++ src/pages/registrocivil/nascimento.tsx
@@ -0,0 +1,9 @@
+import { NascimentoLayout } from "../../components/layouts/nascimento/nascimento";
+
+const Nascimento = () => {
+  return (
+    <NascimentoLayout />
+  );
+}
+
+export default Nascimento;
