ee7d2c745a8f1ca1a30d82fb8ce2c37705b62986
Jonas Brito <jonasexplore@gmail.com>
2022-02-09 12:18:26 -0300

    feat(components): improve person modal

--- src/components/modals/persons/CreatePersonModal.tsx
+++ src/components/modals/persons/CreatePersonModal.tsx
@@ -1,11 +1,13 @@
-import { Button, DatePicker, Form, Input, message, Modal, Steps } from "antd";
-import { useState } from "react";
-import { steps } from "./steps";
-
-const { Step } = Steps
+import React, { useContext } from 'react';
+import { Button, DatePicker, Form, Input, Modal, Row } from "antd";
+import { useEffect, useState } from "react";
+import { Person } from "../../../types/persons";
+import { PersonsContext } from '../../../contexts/persons';
 
 type createPersonModalProps = {
+  personId?: string;
   visible: boolean;
+  createPerson: (person: Person) => void;
   onCancel: () => void;
 };
 
@@ -21,6 +23,7 @@ const formItemLayout = {
 };
 
 const initialValues = {
+  id: "",
   document: "",
   name: "",
   address: "",
@@ -35,62 +38,96 @@ const initialValues = {
 
 const CreatePersonModal = ({
   onCancel,
-  visible
+  visible,
+  personId,
+  createPerson,
 }: createPersonModalProps) => {
-  const [current, setCurrent] = useState(0);
-
-  const next = () => {
-    setCurrent(current + 1);
-  };
-
-  const prev = () => {
-    setCurrent(current - 1);
-  };
-
+  const [editingPerson, setEditingPerson] = useState<Person>(initialValues);
   const [form] = Form.useForm();
+
+  const { persons, updatePerson } = useContext(PersonsContext)
+
+  useEffect(() => {
+    if (personId) {
+      const person = persons.find((person) => person.id === personId);
+
+      if (person) {
+        setEditingPerson(person);
+        form.setFieldsValue(person);
+      }
+    }
+  }, [form, personId, persons])
+
+  const onFinish = (values: any) => {
+    personId ? updatePerson(personId, values) : createPerson(values);
+    onCancel();
+  }
 
   return (
     <Modal
+      destroyOnClose
       title="Cadastro de Pessoas"
       visible={visible}
-      onOk={() => console.log('criar')}
       onCancel={onCancel}
       width={800}
+      footer={null}
     >
       <Form
         {...formItemLayout}
-        initialValues={initialValues}
+        initialValues={editingPerson}
         form={form}
+        onFinish={onFinish}
       >
-        <Steps current={current}>
-          {steps.map(item => (
-            <Step
-              key={item.title}
-              title={item.title}
-              description={item.description}
-              onClick={() => setCurrent(steps.indexOf(item))}
-              style={{ cursor: "pointer" }}
-            />
-          ))}
-        </Steps>
-        <div className="steps-content" style={{ marginTop: '1.5rem' }}>{steps[current].content(Boolean(form.getFieldValue('document')))}</div>
-        <div className="steps-action">
-          {current < steps.length - 1 && (
-            <Button type="primary" onClick={() => next()}>
-              Próximo
+        <Form.Item
+          name="document"
+          label="CPF"
+          wrapperCol={{ span: 6 }}
+        >
+          <Input type="number" />
+        </Form.Item>
+        <Form.Item name="name" label="Nome">
+          <Input />
+        </Form.Item>
+        <Form.Item name="email" label="Email">
+          <Input />
+        </Form.Item>
+        {/* <Form.Item name="birthDate"  label="Data de Nascimento">
+          <DatePicker />
+        </Form.Item> */}
+        <Form.Item name="phone" label="Celular">
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="zip"
+          label="CEP"
+          wrapperCol={{ span: 6 }}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="address" label="Endereço">
+          <Input />
+        </Form.Item>
+        <Form.Item name="city" label="Cidade">
+          <Input />
+        </Form.Item>
+        <Form.Item name="state" label="Estado" wrapperCol={{ span: 6 }}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="country" label="País" wrapperCol={{ span: 6 }}>
+          <Input />
+        </Form.Item>
+        <Row justify='end'>
+          <Form.Item style={{ marginRight: '0.5rem' }}>
+            <Button type="default" onClick={onCancel}>
+              Cancelar
             </Button>
-          )}
-          {current === steps.length - 1 && (
-            <Button type="primary" onClick={() => message.success('Processing complete!')}>
-              Feito
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              {personId ? 'Atualizar' : 'Finalizar'}
             </Button>
-          )}
-          {current > 0 && (
-            <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-              Anterior
-            </Button>
-          )}
-        </div>
+          </Form.Item>
+        </Row>
       </Form>
     </Modal >
   );
